31c4f23b073e88237b254b98bf654ec00f9fc611
Daniel Benmore <danielb@giffgaff.co.uk>
2018-01-04 16:26:02 +0000

    add page-header

---
+++ src/components/gg-page-header/gg-page-header.spec.ts
@@ -0,0 +1,11 @@
+import { ggPageHeader } from './gg-page-header';
+
+describe('my-component', () => {
+  it('should build', () => {
+    expect(new ggCopyBox()).toBeTruthy();
+  });
+
+  describe('rendering', () => {
+
+  });
+});
---
+++ src/components/gg-page-header/gg-page-header.tsx
@@ -0,0 +1,23 @@
+import { Component, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'gg-page-header',
+  styleUrl: 'gg-page-header.scss',
+  shadow: false
+})
+export class ggPageHeader {
+
+  @Prop() header: string;
+
+  render() {
+    return (
+      <header class="page-header-hero page-header-hero--large">
+        <div class="page-header-hero__title">
+           <gg-copybox>
+              <h1 class="txt--display">{ this.header }</h1>
+          </gg-copybox>
+        </div>
+      </header>
+    );
+  }
+}
