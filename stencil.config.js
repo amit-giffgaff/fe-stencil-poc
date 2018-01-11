51fc47ce0331398337bc6d2f742b71a8b1e75fd1
Amitabh Ranjan <amitabh@giffgaff.co.uk>
2018-01-11 17:23:41 +0000

    Angular 4 integeration

--- stencil.config.js
+++ stencil.config.js
@@ -2,7 +2,7 @@ exports.config = {
   namespace: 'components',
   generateDistribution: true,
   bundles: [
-    { components: ['ggc-copybox'] }
+    { components: ['ggc-textbox', 'ggc-page-header', 'ggc-button', 'ggc-copybox'] }
   ]
 };
 
